f656ae3cff9d0f7be9a9dbfa6755d844e742341c
Iqbal Pamula <balpamula@gmail.com>
2025-11-08 03:51:02 +0700

    feat: :sparkles: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { AppProviders } from "@/providers";
+import { ThemeSwitcher } from "@/components/theme";
+
 export default function App() {
   return (
-    <div className="flex h-screen w-full flex-row items-center justify-center">
-      <h1 className="text-2xl">Hello World!</h1>
-    </div>
+    <AppProviders>
+      <div className="flex h-screen w-full flex-row items-center justify-center gap-4">
+        <h1 className="text-2xl">Hello World!</h1>
+        <ThemeSwitcher />
+      </div>
+    </AppProviders>
   );
 }
