78815e751648484d5f479e14a4fc8e8c51018295
Iqbal Pamula <balpamula@gmail.com>
2025-11-08 01:47:49 +0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen w-full flex-row items-center justify-center">
+      <h1 className="text-2xl">Hello World!</h1>
+    </div>
+  );
+}
