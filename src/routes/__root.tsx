d5ddc193722dafcc65fb6c222a0df441784bc8df
Iqbal Pamula <balpamula@gmail.com>
2025-12-25 13:17:44 +0700

    feat: :sparkles: restructure routes and update about page to use nested routing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,23 @@
 import { ThemeSwitcher } from "@/components/theme";
+import { Button } from "@/components/ui/button";
 import { AppProviders } from "@/providers";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+const links = [
+  { to: "/", label: "Home" },
+  { to: "/about", label: "About" },
+];
+
 const RootLayout = () => (
   <AppProviders>
     <div className="container mx-auto flex h-14 w-full flex-row items-center justify-between">
-      <div className="flex flex-row items-center space-x-4">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+      <div className="flex flex-row items-center">
+        {links.map(({ to, label }) => (
+          <Button asChild key={to} variant="ghost">
+            <Link to={to}>{label}</Link>
+          </Button>
+        ))}
       </div>
       <ThemeSwitcher />
     </div>
